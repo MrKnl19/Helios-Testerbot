0513b9df8d070df4d41d7d95a5ea2179b7bdc6ef
MrKartikL2 <115404704+MrKartikL2@users.noreply.github.com>
2022-10-10 12:50:40 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -158,7 +158,7 @@ def get_readable_message():
                 msg += f"\n<b>├ Source:</b> <a href='{download.message.link}'>{uname}</a> | <b>Id :</b> <code>{download.message.from_user.id}</code>"
             else:
                 msg += ''
-            msg += f"\n<b>├ To Cancel:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+            msg += f"\n<b>╰ To Cancel:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
